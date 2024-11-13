9c90b5215f9275ee81aef3cc86ca6100267e7cc7
IDO <susususu07@naver.com>
2024-11-13 16:56:09 +0900

    Add files via upload

---
+++ out/core/src/main/java/hello/core/member/MemberSerrviceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+// 관례: 인터페이스에 관한 구현체가 하나만 있을 경우
+// 클래스 이름을 +Impl로 적어준다
+public class MemberSerrviceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    public MemberSerrviceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
