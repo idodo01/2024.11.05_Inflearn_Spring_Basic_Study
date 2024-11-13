f50ddd9d3e2c1c7e4c2eb6bd39e1f12d51406871
IDO <susususu07@naver.com>
2024-11-13 18:16:30 +0900

    Add files via upload

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberSerrviceImpl;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +12,19 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberSerrviceImpl(new MemoryMemberRepository());
+        return new MemberSerrviceImpl(memoryMemberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memoryMemberRepository(), fixDiscountPolicy());
+    }
+
+    public MemoryMemberRepository memoryMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy fixDiscountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
